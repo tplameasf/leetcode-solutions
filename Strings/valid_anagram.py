63a59fc9f1752eb34f9f377a46c7ed46972f4e1a
Tirthu ⭐️ <tn0669@srmist.edu.in>
2025-12-01 21:57:05 +0530

    contain valid anagram problem

---
+++ Strings/valid_anagram.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        if len(s)!=len(t):
+            return False
+        countS={}
+        countT={}
+        for char in s:
+            countS[char]=countS.get(char,0)+1
+        for char in t:
+            countT[char]=countT.get(char,0)+1
+        return countS==countT
