1f83b07f67c405f545a2bcbd608d9f5ae3cc41d0
Tirthu ⭐️ <tn0669@srmist.edu.in>
2025-12-02 15:04:31 +0530

    create majority_elements.py

---
+++ Hashmaps/majority_elements.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        count=0
+        candidate=1
+        for num in nums:
+            if count==0:
+                candidate=num
+            count+=1 if num==candidate else-1
+        return candidate
