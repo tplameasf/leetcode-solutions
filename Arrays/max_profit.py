7fc0d23a75f7c93a7f8c61ceeee29650d75fdc4e
Tirthu ⭐️ <tn0669@srmist.edu.in>
2025-12-03 16:00:47 +0530

    found max profit

---
+++ Arrays/max_profit.py
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int min_price = INT_MAX;
+        int max_profit = 0;
+
+        for (int price : prices) {
+            if (price < min_price) {
+                min_price = price;
+            } else {
+                max_profit = max(max_profit, price - min_price);
+            }
+        }
+
+        return max_profit;
+    }
+};
